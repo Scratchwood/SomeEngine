5350043c40d4a6a0a17ee1221d10bab4a14aee86
Emil Ekberg <ekberg.emil@gmail.com>
2018-01-14 00:18:02 +0100

    added vector3 and updated vector2. some weird files are aso here.

--- SomeEngine/SomeEngine/src/math/Vector2.cpp
+++ SomeEngine/SomeEngine/src/math/Vector2.cpp
@@ -1,106 +1,114 @@
 #include "Vector2.h"
 
-namespace engine 
+namespace engine::math 
 {
-	namespace math 
+	Vector2::Vector2() :x(0), y(0)
 	{
-		Vector2::Vector2() :x(0), y(0)
-		{
 
-		}
+	}
 
-		Vector2::Vector2(const float x, const float y) : x(x), y(y)
-		{
+	Vector2::Vector2(const float x, const float y) : x(x), y(y)
+	{
 
-		}
+	}
 
-		Vector2::Vector2(const Vector2 & v) : x(v.x), y(v.y)
-		{
+	Vector2::Vector2(const Vector2 & v) : x(v.x), y(v.y)
+	{
 
-		}
+	}
 
-		float Vector2::length()
-		{
-			return sqrtf((x*x) + (y*y));
-		}
+	float Vector2::length()
+	{
+		return sqrtf((x*x) + (y*y));
+	}
 
-		float Vector2::dot(const Vector2 & right) const
-		{
-			return (this->x * right.x) + (this->y * right.y);
-		}
+	float Vector2::dot(const Vector2 & right) const
+	{
+		return (x * right.x) + (y * right.y);
+	}
 
-		void Vector2::normalize()
-		{
-			float l = length();
-			x /= l;
-			y /= l;
-		}
+	void Vector2::normalize()
+	{
+		float l = length();
+		x /= l;
+		y /= l;
+	}
 
-		Vector2 & Vector2::operator+=(const Vector2 & right)
-		{
-			this->x += right.x;
-			this->y += right.y;
-			return *this;
-		}
+	Vector2 & Vector2::operator+=(const Vector2 & right)
+	{
+		x += right.x;
+		y += right.y;
+		return *this;
+	}
 
-		Vector2 & Vector2::operator-=(const Vector2 & right)
-		{
-			this->x -= right.x;
-			this->y -= right.y;
-			return *this;
-		}
+	Vector2 & Vector2::operator-=(const Vector2 & right)
+	{
+		x -= right.x;
+		y -= right.y;
+		return *this;
+	}
 
-		Vector2 & Vector2::operator*=(const Vector2 & right)
-		{
-			this->x *= right.x;
-			this->y *= right.y;
-			return *this;
-		}
+	Vector2 & Vector2::operator*=(const Vector2 & right)
+	{
+		x *= right.x;
+		y *= right.y;
+		return *this;
+	}
 
-		Vector2 & Vector2::operator/=(const Vector2 & right)
-		{
-			this->x /= right.x;
-			this->y /= right.y;
-			return *this;
-		}
+	Vector2 & Vector2::operator/=(const Vector2 & right)
+	{
+		x /= right.x;
+		y /= right.y;
+		return *this;
+	}
 
-		bool Vector2::operator==(const Vector2 & right)
-		{
-			return (this->x == right.x) && (this->y == right.y);
-		}
+	bool Vector2::operator==(const Vector2 & right)
+	{
+		return (x == right.x) && (y == right.y);
+	}
 
-		bool Vector2::operator!=(const Vector2 & right)
-		{
-			return !(*this==right);
-		}
-		Vector2 operator+(Vector2 left, const Vector2 & right)
-		{
-			return Vector2(left.x + right.x, left.y + right.y);
-		}
+	bool Vector2::operator!=(const Vector2 & right)
+	{
+		return !(*this==right);
+	}
+	Vector2 operator+(Vector2 left, const Vector2 & right)
+	{
+		return Vector2(left.x + right.x, left.y + right.y);
+	}
 
-		Vector2 operator-(Vector2 left, const Vector2 & right)
-		{
-			return Vector2(left.x - right.x, left.y - right.y);
-		}
+	Vector2 operator-(Vector2 left, const Vector2 & right)
+	{
+		return Vector2(left.x - right.x, left.y - right.y);
+	}
 
-		Vector2 operator*(Vector2 left, const Vector2 & right)
-		{
-			return Vector2(left.x * right.x, left.y * right.y);
-		}
+	Vector2 operator*(Vector2 left, const Vector2 & right)
+	{
+		return Vector2(left.x * right.x, left.y * right.y);
+	}
 
-		Vector2 operator*(Vector2 left, const float right)
-		{
-			return Vector2(left.x * right, left.y * right);
-		}
+	Vector2 operator*(Vector2 left, const float right)
+	{
+		return Vector2(left.x * right, left.y * right);
+	}
 
-		Vector2 operator/(Vector2 left, const Vector2 & right)
-		{
-			return Vector2(left.x / right.x, left.y / right.y);
-		}
+	Vector2 operator/(Vector2 left, const Vector2 & right)
+	{
+		return Vector2(left.x / right.x, left.y / right.y);
+	}
 
-		std::ostream & operator<<(std::ostream & stream, const Vector2 & vector)
-		{
-			return stream << "Vector2(X: " << vector.x << ", Y: " << vector.y << ")";
-		}
+	Vector2 operator/(Vector2 left, const float right)
+	{
+		return Vector2(left.x / right, left.y / right);
+	}
+
+	void Vector2::operator=(const Vector2& right)
+	{
+		x = right.x;
+		y = right.y;
+	}
+
+	std::ostream & operator<<(std::ostream & stream, const Vector2 & vector)
+	{
+		return stream << "Vector2(X: " << vector.x << ", Y: " << vector.y << ")";
 	}
 }
--- SomeEngine/SomeEngine/src/math/Vector2.h
+++ SomeEngine/SomeEngine/src/math/Vector2.h
@@ -1,38 +1,37 @@
 #pragma once
 #include <iostream>
-
-namespace engine 
+namespace engine::math
 {
-	namespace math 
+	struct Vector2 
 	{
-		struct Vector2 
-		{
-			float x, y;
+		float x, y;
 			
-			Vector2();
-			Vector2(const float x, const float y);
-			Vector2(const Vector2& v);
+		Vector2();
+		Vector2(const float x, const float y);
+		Vector2(const Vector2& v);
 
-			float length();
-			float dot(const Vector2& right) const;
+		float length();
+		float dot(const Vector2& right) const;
 
-			void normalize();
+		void normalize();
 
-			friend Vector2 operator+(Vector2 left, const Vector2& right);
-			friend Vector2 operator-(Vector2 left, const Vector2& right);
-			friend Vector2 operator*(Vector2 left, const Vector2& right);
-			friend Vector2 operator*(Vector2 left, const float right);
-			friend Vector2 operator/(Vector2 left, const Vector2& right);
+		friend Vector2 operator+(Vector2 left, const Vector2& right);
+		friend Vector2 operator-(Vector2 left, const Vector2& right);
+		friend Vector2 operator*(Vector2 left, const Vector2& right);
+		friend Vector2 operator*(Vector2 left, const float right);
+		friend Vector2 operator/(Vector2 left, const Vector2& right);
+		friend Vector2 operator/(Vector2 left, const float right);
 
-			Vector2& operator+=(const Vector2& right);
-			Vector2& operator-=(const Vector2& right);
-			Vector2& operator*=(const Vector2& right);
-			Vector2& operator/=(const Vector2& right);
+		Vector2& operator+=(const Vector2& right);
+		Vector2& operator-=(const Vector2& right);
+		Vector2& operator*=(const Vector2& right);
+		Vector2& operator/=(const Vector2& right);
 
-			bool operator==(const Vector2& right);
-			bool operator!=(const Vector2& right);
+		bool operator==(const Vector2& right);
+		bool operator!=(const Vector2& right);
 
-			friend std::ostream& operator<<(std::ostream& stream, const Vector2& vector);
-		};
-	}
+		void operator=(const Vector2& right);
+
+		friend std::ostream& operator<<(std::ostream& stream, const Vector2& vector);
+	};
 }
